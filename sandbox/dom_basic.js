2f5b217c082d4bf29d39954edaae9b1fa3105e07
Michael <olu21003@byui.edu>
2024-09-19 09:59:38 -0600

    dom class practices

---
+++ sandbox/dom_basic.js
@@ -0,0 +1,7 @@
+// const newParagraph = document.createElement("p");
+// newParagraph.innerText = "Added with Javascript!";
+// document.body.appendChild(newParagraph);
+
+const newImg = document.createElement("img");
+newImg.src = "https://picsum.photos/200/300";
+document.body.append(newImg);
