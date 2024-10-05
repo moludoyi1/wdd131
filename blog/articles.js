4092e6ed7d9e4777c756860c1598d6917f01d71a
Michael <olu21003@byui.edu>
2024-10-04 19:35:31 -0600

    all done

--- blog/articles.js
+++ blog/articles.js
@@ -1,28 +1,30 @@
-import{articles} from  './blog.js'; // import articles from blog.js
+import { articles } from "./blog.js"; // import articles from blog.js
 
 // Function to build articles from the array
-    function displayArticles() {
-        const articlesContainer = document.querySelector('.book-review');
+function displayArticles() {
+  const articlesContainer = document.querySelector(".book-review");
 
-        articles.forEach((article) => {
-            // Create a new article element
-            const articleElement = document.createElement("article");
+  articles.forEach((article) => {
+    // Create a new article element
+    const articleElement = document.createElement("article");
 
-            // Template literal for article HTML
-            articleElement.innerHTML = `
-                <h2>${article.title}</h2>
-                <p class="date">${article.date}</p>
-                <ul class="details-container">
-                    <li>Age: ${article.ages}</li>
-                    <li>Genre: ${article.genre}</li>
-                </ul>
-                <img src="${article.imgSrc}" alt="${article.imgAlt}">
-                <p>${article.description}</p>
-            `;
+    // Template literal for article HTML
+    articleElement.innerHTML = `
+      <ul class="details-container">
+      <p class="date">${article.date}</p>
+        <li>Age: ${article.ages}</li>
+        <li>Genre: ${article.genre}</li>
+      </ul>
+      <div>
+        <h2>${article.title}</h2>
+        <img src="${article.imgSrc}" alt="${article.imgAlt}">
+        <p>${article.description}</p>
+      </div>
+      `;
 
-        // Append the new article to the container
-        articlesContainer.appendChild(articleElement);
-        });
-    }
+    // Append the new article to the container
+    articlesContainer.appendChild(articleElement);
+  });
+}
 
 document.addEventListener("DOMContentLoaded", displayArticles);
